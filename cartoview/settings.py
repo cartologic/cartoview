a30e675baaa817fa97a840d247f584fca66bf9aa
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2016-02-17 11:15:53 +0200

    add apps folder to sys.path

--- cartoview/settings.py
+++ cartoview/settings.py
@@ -94,7 +94,6 @@ INSTALLED_APPS = tuple(i for i in INSTALLED_APPS if i not in GEONODE_APPS)
 INSTALLED_APPS += ('cartoview.app_manager', 'cartoview.apps')
 INSTALLED_APPS += GEONODE_APPS
 
-
 # auto load apps
 from cartoview.app_manager.apps_helper import get_apps_names, APPS_DIR
 
@@ -102,19 +101,20 @@ CARTOVIEW_APPS = ()
 
 import importlib, sys
 
-# sys.path.append(os.path.abspath(os.path.join(LOCAL_ROOT, 'apps')))
+# TODO: find better solution to get access to apps directly.
+sys.path.append(os.path.abspath(os.path.join(LOCAL_ROOT, 'apps')))
 
 for app_name in get_apps_names():
-	try:
-		CARTOVIEW_APPS += ('cartoview.apps.' + app_name,)
-		# settings_module = importlib.import_module('apps.%s.settings' % app_name)
-		app_settings_file = os.path.join(APPS_DIR, app_name, 'settings.py')
-		if os.path.exists(app_settings_file):
-			# By doing this instead of import, app/settings.py can refer to
-			# local variables from settings.py without circular imports.
-			execfile(app_settings_file)
-	except:
-		pass
+    try:
+        CARTOVIEW_APPS += ('cartoview.apps.' + app_name,)
+        # settings_module = importlib.import_module('apps.%s.settings' % app_name)
+        app_settings_file = os.path.join(APPS_DIR, app_name, 'settings.py')
+        if os.path.exists(app_settings_file):
+            # By doing this instead of import, app/settings.py can refer to
+            # local variables from settings.py without circular imports.
+            execfile(app_settings_file)
+    except:
+        pass
 
 INSTALLED_APPS += CARTOVIEW_APPS
 
