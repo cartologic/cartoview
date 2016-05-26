36a71a773fe4d8c8d30066a120c2266e610c8cf7
Kamal Alseisy <kamal.alseisy@cartologic.com>
2016-05-26 13:51:30 +0200

    add maps list view

--- cartoview/viewer/urls.py
+++ cartoview/viewer/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
+   url(r'^maps/$', views.list_maps, name='viewer.list_maps'),
    url(r'^maps/(?P<map_id>\d+)/view/$', views.view_map, name='viewer.view_map'),
    url(r'^maps/(?P<map_id>\d+)/embed/$', views.embed_map, name='viewer.embed_map'),
    url(r'^maps/(?P<map_id>\d+)/config.json', views.map_config, name='viewer.map_config'),
