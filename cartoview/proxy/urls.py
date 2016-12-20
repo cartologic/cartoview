1880c94189ff18392969a5e8fc07122c87a015e8
Hisham Karam <hisham.karam@cartologic.com>
2016-12-20 10:56:17 +0200

    remove proxy fake url

--- cartoview/proxy/urls.py
+++ cartoview/proxy/urls.py
@@ -4,7 +4,6 @@ from views import proxy_view
 
 urlpatterns = patterns(
         '',
-        url(r'^http://localhost:4041/(?P<url_name>[^/]+)/(?P<sub_url>.*)$', proxy_view, name='fake_cartoview_proxy'),
         url(r'^(?P<url_name>[^/]+)/(?P<sub_url>.*)$', proxy_view, name='cartoview_proxy'),
 )
 
