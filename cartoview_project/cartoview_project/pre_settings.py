8cccfea86920dcce9b69495e06b5a0f637af0f52
Kamal Alseisy <kamal.alseisy@cartologic.com>
2017-01-26 14:59:56 +0200

    add cartoview sample project

---
+++ cartoview_project/cartoview_project/pre_settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+# uncomment the following in windows and set the GEDAL_DIR
+# GDAL_DIR = "path/to/gdal"
+# os.environ["Path"] = GDAL_DIR + ";" + os.environ["Path"]
+# os.environ["GEOS_LIBRARY_PATH"] = os.path.join(GDAL_DIR, "geos_c.dll")
+# os.environ["GDAL_LIBRARY_PATH"] = os.path.join(GDAL_DIR, "gdal111.dll")
