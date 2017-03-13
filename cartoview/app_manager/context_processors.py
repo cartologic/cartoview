016895576f04af89597a792c22a65377fbb3caca
Hisham Karam <hisham.karam@cartologic.com>
2017-03-13 14:43:27 +0200

    add news in menu

--- cartoview/app_manager/context_processors.py
+++ cartoview/app_manager/context_processors.py
@@ -1,7 +1,9 @@
 from geonode.groups.models import Group, GroupProfile
 from cartoview.app_manager.models import App
+from django.conf import settings
 
-
+def news(requets):
+    return {'news_app': 'cartoview_news' in settings.INSTALLED_APPS}
 
 def apps(requets):
     return {'apps': App.objects.all().order_by('order')}
--- cartoview/settings.py
+++ cartoview/settings.py
@@ -8,5 +8,5 @@ GEONODE_ROOT = os.path.abspath(os.path.dirname(geonode.__file__))
 TEMPLATES[0]["DIRS"] = [os.path.join(CARTOVIEW_ROOT, "templates")] + TEMPLATES[0]["DIRS"]
 STATICFILES_DIRS += [os.path.join(CARTOVIEW_ROOT, "static"),]
 cartoview_apps_settings_path = os.path.join(CARTOVIEW_ROOT, 'app_manager', "settings.py")
-TEMPLATES[0]["OPTIONS"]['context_processors'] += ('cartoview.app_manager.context_processors.apps',)
+TEMPLATES[0]["OPTIONS"]['context_processors'] += ('cartoview.app_manager.context_processors.apps','cartoview.app_manager.context_processors.news')
 execfile(cartoview_apps_settings_path)
