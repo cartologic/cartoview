44f3e68d67f6aa1e17f12dc2b704fb05c7cdfd5f
Hisham Karam <hisham.karam@cartologic.com>
2017-03-13 15:54:43 +0200

    version 1.1.19

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 setup(
   name = 'cartoview',
   packages = find_packages(),
-  version = '1.1.18',
+  version = '1.1.19',
   description = 'Cartoview is a GIS web mapping application framework to easily share and deploy apps based on Geonode',
   long_description=long_description,
   author = 'Cartologic',
