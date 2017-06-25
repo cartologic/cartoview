b1ada0dd6839682a967db8d643ba17419306486c
hisham waleed karam <hisham.karam@cartologic.com>
2017-06-25 02:50:18 +0200

    fix minor bugs
    close A tag For Help Element

--- cartoview/views.py
+++ cartoview/views.py
@@ -3,6 +3,7 @@ import requests
 from geonode.version import get_version
 from cartoview import __version__
 
+
 def index(request):
     context = {}
     return render(request, 'site_index.html', context)
@@ -11,4 +12,4 @@ def index(request):
 def check_version(request):
     r = requests.get("http://pypi.python.org/pypi/cartoview/json")
     context = dict(latest_version=r.json()["info"]["version"], current_version=get_version(__version__))
-    return render(request, "cartoview/check_version.js", context=context,content_type="text/javascript")
+    return render(request, "cartoview/check_version.js", context=context, content_type="text/javascript")
