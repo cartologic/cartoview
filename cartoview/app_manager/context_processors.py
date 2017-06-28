699791495cfcfc5c57291567d6487f07a37f9f1f
hisham waleed karam <hisham.karam@cartologic.com>
2017-06-28 12:54:08 +0200

    fix minor bugs in responsiveness

--- cartoview/app_manager/context_processors.py
+++ cartoview/app_manager/context_processors.py
@@ -4,16 +4,16 @@ from cartoview.app_manager.models import App, AppInstance, Logo
 from django.conf import settings
 
 
-def news(requets):
+def news(request):
     return {'news_app': 'cartoview_news' in settings.INSTALLED_APPS}
 
 
-def apps(requets):
+def apps(request):
     return {'apps': App.objects.all().order_by('order')}
-def apps_menu(requets):
+def apps_menu(request):
     return {'APPS_MENU': settings.APPS_MENU}
 
-def apps_instance(requets):
+def apps_instance(request):
     instances = AppInstance.objects.all()
     num = AppInstance.objects.all().count()
     return {'apps_instance_count': num, 'instances': instances.order_by('app__order')[:5]}
@@ -23,7 +23,7 @@ from django.contrib.sites.models import Site
 from django.shortcuts import get_list_or_404, get_object_or_404
 
 
-def site_logo(requets):
+def site_logo(request):
     try:
         logo = get_object_or_404(Logo, site=Site.objects.get_current())
         return {'site_logo': logo}
