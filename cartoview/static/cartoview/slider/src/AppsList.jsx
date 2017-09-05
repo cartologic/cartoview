680981317fd811c31299c8b91e85a1b313865a3f
hisham waleed karam <hisham.karam@cartologic.com>
2017-09-05 09:13:47 +0200

    Collapsible apps and tools list in side panel

--- cartoview/static/cartoview/slider/src/AppsList.jsx
+++ cartoview/static/cartoview/slider/src/AppsList.jsx
@@ -7,79 +7,100 @@ import Spinner from 'react-spinkit'
 import { render } from 'react-dom'
 
 export default class AppsList extends Component {
-    constructor( props ) {
-        super( props )
+    constructor(props) {
+        super(props)
         this.state = {
-            apps: [ ],
-            tools:[],
+            apps: [],
+            tools: [],
             loading: true
         }
     }
-    loadApps = ( ) => {
+    loadApps = () => {
         const url = "/api/app/?single_instance=false"
-        fetch( url ).then( ( response ) => response.json( ) ).then( ( data ) => {
-            this.setState( {
-                apps: data.objects.sort( ( a, b ) => {
-                    if ( a.order < b.order ) {
+        fetch(url).then((response) => response.json()).then((data) => {
+            this.setState({
+                apps: data.objects.sort((a, b) => {
+                    if (a.order < b.order) {
                         return -1
                     }
-                    if ( a.order > b.order ) {
+                    if (a.order > b.order) {
                         return 1
                     }
-                } ),
+                }),
                 loading: false
-            } )
-        } )
+            })
+        })
     }
-    loadTools = ( ) => {
+    loadTools = () => {
         const url = "/api/app/?single_instance=true"
-        fetch( url ).then( ( response ) => response.json( ) ).then( ( data ) => {
-            this.setState( {
-                tools: data.objects.sort( ( a, b ) => {
-                    if ( a.order < b.order ) {
+        fetch(url).then((response) => response.json()).then((data) => {
+            this.setState({
+                tools: data.objects.sort((a, b) => {
+                    if (a.order < b.order) {
                         return -1
                     }
-                    if ( a.order > b.order ) {
+                    if (a.order > b.order) {
                         return 1
                     }
-                } ),
+                }),
                 loading: false
-            } )
-        } )
+            })
+        })
     }
-    componentWillMount( ) {
-        this.loadApps( )
+    componentWillMount() {
+        this.loadApps()
         this.loadTools()
     }
-    render( ) {
-        let { apps, loading,tools } = this.state
+    render() {
+        let { apps, loading, tools } = this.state
         return (
             <div>
-                <h3>Applications</h3>
-                <hr />
-                {apps.length > 0 && <ul className="list-group">
-                    {apps.map((app, i) => {
-                        return <a key={i} href="javascript:;"><li  onClick={()=>window.location.replace(`/apps/appinstances/?app__title=${app.title}`)} key={i} className="list-group-item">
-                            {app.title}
-                            <span className="badge pull-right">{app.app_instance_count}</span></li></a>
-                    })}
-                </ul>}
+
+                {apps.length > 0 && <div className="panel-group">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h4 className="panel-title">
+                                <a data-toggle="collapse" href="#collapse1">Applications</a>
+                            </h4>
+                        </div>
+                        <div id="collapse1" className="panel-collapse collapse in">
+                            <ul className="list-group">
+                                {apps.map((app, i) => {
+                                    return <a key={i} href="javascript:;"><li onClick={() => window.location.replace(`/apps/appinstances/?app__title=${app.title}`)} key={i} className="list-group-item">
+                                        {app.title}
+                                        <span className="badge pull-right">{app.app_instance_count}</span></li></a>
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>}
+
                 {loading && <Spinner name="line-scale" />}
                 {!loading && apps.length == 0 && <small>No Apps installed</small>}
                 <hr />
-                <h3>Tools</h3>
-                <hr />
-                {tools.length > 0 && <div className="row"><Equalizer>
-                    {tools.map((tool, i) => {
-                        return <div  key={i}  className="col-xs-4 col-sm-4 col-md-4 col-lg-4"><a href="javascript:;" onClick={()=>window.location.replace(`/apps/${tool.name}/`)}>
-                            <img className="img-responsive img-circle" src={`/static/${tool.name}/logo.png`} />
-                            <p style={{ wordBreak: "normal",textAlign:'center'}}>{tool.title}</p>
-                            </a>
-                        </div>
-                    
-                    })}
 
-                </Equalizer></div>}
+                {tools.length > 0 && <div className="panel-group">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h4 className="panel-title">
+                                <a data-toggle="collapse" href="#collapse2">Tools</a>
+                            </h4>
+                        </div>
+                        <div  id="collapse2" className="panel-collapse collapse in">
+                            <Equalizer byRow={false} property="height">
+                                {tools.map((tool, i) => {
+                                    return <div key={i} className="col-xs-6 col-sm-6 col-md-6 col-lg-6 text-center"><a href="javascript:;" onClick={() => window.location.replace(`/apps/${tool.name}/`)}>
+                                        <img className="img-responsive img-circle" src={`/static/${tool.name}/logo.png`} />
+                                        <p style={{ wordBreak: "normal", textAlign: 'center' }}>{tool.title}</p>
+                                    </a>
+                                    </div>
+
+                                })}
+
+                            </Equalizer>
+                        </div>
+                    </div>
+                </div>}
                 {loading && <Spinner name="line-scale" />}
                 {!loading && tools.length == 0 && <small>No Tools installed</small>}
                 <hr />
@@ -87,4 +108,4 @@ export default class AppsList extends Component {
         )
     }
 }
-render( <AppsList />, document.getElementById( "react-apps-list" ) )
+render(<AppsList />, document.getElementById("react-apps-list"))
