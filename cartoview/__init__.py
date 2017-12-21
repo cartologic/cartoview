180eb3c5de872862f4434308cacaf0a002a22f59
hisham waleed karam <hisham.karam@cartologic.com>
2017-12-21 10:15:18 +0200

    1.4.2

--- cartoview/__init__.py
+++ cartoview/__init__.py
@@ -1,4 +1,5 @@
-__version__ = (1, 4, 1, 'final', 0)
+__version__ = (1, 4, 2, 'final', 0)
 __compatible_with__ = [
-    (1, 4, 0, 'final', 0)
+    (1, 4, 0, 'final', 0),
+    (1, 4, 1, 'final', 0)
 ]
