8fd6751369826d1bb99eed7e431047aedd14e206
hisham waleed karam <hisham.karam@cartologic.com>
2018-02-14 10:11:07 +0200

    add app_instance logo migration file

---
+++ cartoview/app_manager/migrations/0008_appinstance_logo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import cartoview.app_manager.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app_manager', '0007_auto_20180128_0116'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='appinstance',
+            name='logo',
+            field=models.ImageField(null=True, upload_to=cartoview.app_manager.models.get_app_logo_path, blank=True),
+        ),
+    ]
