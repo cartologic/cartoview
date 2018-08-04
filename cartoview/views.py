b93ca1e230043d5c91898ada1ea31e20e74116e2
hisham waleed karam <hisham.karam@cartologic.com>
2018-08-05 00:28:36 +0200

    return js compatible version in check_version

--- cartoview/views.py
+++ cartoview/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 from django.shortcuts import render
+from pkg_resources import parse_version
 
 from .log_handler import get_logger
 from .version import get_current_version
@@ -15,9 +16,13 @@ def index(request):
 
 def check_version(request):
     r = requests.get("https://pypi.org/pypi/cartoview/json")
+    _version = parse_version(get_current_version())._version
+    release = _version.release
+    version = [str(x) for x in release]
+    current_version = ".".join(version)
     context = dict(
         latest_version=r.json()["info"]["version"],
-        current_version=get_current_version())
+        current_version=current_version)
     return render(
         request,
         "cartoview/check_version.js",
