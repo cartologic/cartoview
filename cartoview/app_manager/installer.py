7693248967245fec8abd7f475d6c75d84c180753
hisham waleed karam <hisham.karam@cartologic.com>
2018-08-04 20:55:39 +0200

    better error handling

--- cartoview/app_manager/installer.py
+++ cartoview/app_manager/installer.py
@@ -149,13 +149,9 @@ class AppInstaller(object):
         """
         get app information form app store rest url
         """
-        try:
-            q = requests.get(self.store.url + ''.join(
-                [str(item) for item in args]))
-            return q.json()
-        except Exception as e:
-            logger.error(e.message)
-            return None
+        q = requests.get(self.store.url + ''.join(
+            [str(item) for item in args]))
+        return q.json()
 
     def extract_move_app(self, zipped_app):
         extract_to = tempfile.mkdtemp(dir=temp_dir)
@@ -180,7 +176,7 @@ class AppInstaller(object):
             if not os.access(temp_dir, os.W_OK):
                 change_path_permission(temp_dir)
             self.extract_move_app(zip_ref)
-        except Exception as e:
+        except shutil.Error as e:
             raise e
         finally:
             zip_ref.close()
