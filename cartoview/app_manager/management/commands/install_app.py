bdb29161e580c7f039c8f3549c33e31c6b841135
hisham waleed karam <hisham.karam@cartologic.com>
2018-09-20 18:38:23 +0200

    add install_app command

---
+++ cartoview/app_manager/management/commands/install_app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from cartoview.app_manager.installer import AppInstaller
+from cartoview.app_manager.models import AppStore
+from django.core.management.base import BaseCommand
+from cartoview.log_handler import get_logger
+logger = get_logger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Install App'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-n', '--name', dest='name', help='Name Of The App To Install')
+        parser.add_argument(
+            '-av', '--app_version', type=str, dest='app_version', help='App Version')
+
+    def handle(self, *args, **options):
+        app_name = options.get('name')
+        app_version = options.get('app_version')
+        store = AppStore.objects.get(is_default=True)
+        try:
+            installer = AppInstaller(app_name, store.id, app_version)
+            installer.install()
+        except Exception as ex:
+            logger.error(ex.message)
