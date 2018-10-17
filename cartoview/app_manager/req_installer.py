2d71995c1a136c4de3957b8cccf2b8808b19b876
hisham waleed karam <hisham.karam@cartologic.com>
2018-10-17 14:03:54 +0200

    use subprocess instead of pip package

--- cartoview/app_manager/req_installer.py
+++ cartoview/app_manager/req_installer.py
@@ -2,39 +2,25 @@
 import os
 from os import R_OK, access
 
-try:
-    from pip import main
-except:
-    from pip._internal import main
+
+class ReqInstallerException(Exception):
+    pass
 
 
 class ReqInstaller(object):
-    def __init__(self, reqfile):
+    def __init__(self, reqfile, target=None):
+        if not os.path.exists(reqfile):
+            raise ReqInstallerException("requirement file doesn't exists!")
+        if not access(reqfile, R_OK):
+            raise ReqInstallerException("can not read requirement file!")
         self.file = reqfile
         self.requirements = []
+        self.target = target
         self.preprocess_reqfile()
 
-    def _install_req(self, package, *args, **kwargs):
-        main_args = ['install', package]
-        main_args.extend(self._get_flags(*args, **kwargs))
-        main(main_args)
-
-    def _get_flags(self, *args, **kwargs):
-        flags = []
-        for k, v in kwargs.iteritems():
-            if k == "no_cache" and v:
-                flags.append("--no-cache-dir")
-        return flags
-
-    def preprocess_reqfile(self):
-        if not os.path.exists(self.file):
-            raise Exception("requirement file doesn't exists!")
-        if not access(self.file, R_OK):
-            raise Exception("can not read requirement file!")
-        with open(self.file, 'r') as reqfile:
-            lines = reqfile.readlines()
-            self.requirements = [x.strip(" ") for x in lines]
-
     def install_all(self, *args, **kwargs):
-        for package in self.requirements:
-            self._install_req(package, *args, **kwargs)
+        from subprocess import call
+        command = ["pip", "install", "-r", self.file]
+        if self.target:
+            command.extend(["-t", self.target])
+        call(command)
