6c7b1d05122ba07979dec6058b616d702fd548c1
hisham waleed karam <hisham.karam@cartologic.com>
2018-10-28 14:10:31 +0200

    add test for version module

---
+++ cartoview/tests/test_version_module.py
@@ -0,0 +1,29 @@
+from builtins import basestring
+
+from django.test import TestCase
+
+from cartoview.version import get_version, json_version_info
+
+
+class CartoviewVersionModule(TestCase):
+
+    def test_json_version_info(self):
+        version_text = json_version_info()
+        self.assertTrue(isinstance(version_text, basestring))
+        self.assertTrue(
+            ("current_version" in version_text and
+             "backward_versions" in version_text))
+
+    def test_get_version(self):
+        stable = (1, 8, 5, 'final', 0)
+        unstable = (1, 8, 5, 'unstable', 0)
+        rc = (1, 8, 5, 'rc', 0)
+        beta = (1, 8, 5, 'beta', 0)
+        version = get_version(stable)
+        self.assertEqual(version, '1.8.5')
+        version = get_version(rc)
+        self.assertEqual(version, '1.8.5rc0')
+        version = get_version(beta)
+        self.assertEqual(version, '1.8.5b0')
+        version = get_version(unstable)
+        self.assertTrue("dev" in version)
