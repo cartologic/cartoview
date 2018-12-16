b6ca4e11570f423bf0f8ac0915e6cb79b4651837
hisham waleed karam <hisham.karam@cartologic.com>
2018-12-16 22:30:48 +0200

    improve docker image

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -1,35 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+apt-get update -y && apt-get install wget gnupg -y
+
+# add postgres client latest
+touch /etc/apt/sources.list.d/pgdg.list &&
+	echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" >>/etc/apt/sources.list.d/pgdg.list &&
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+# add gdal repo
+echo "deb http://http.us.debian.org/debian buster main non-free contrib" >>/etc/apt/sources.list
 
 # geonode required libraries
-apt-get update && apt-get install -y \
-	build-essential gcc \
-	git wget gnupg \
-	gettext \
-	sqlite3 libxml2-dev libxslt1-dev \
+apt-get update -y && apt-get install -y \
+	build-essential gcc git \
+	libxml2-dev libxslt1-dev libxslt-dev python-dev \
+	gettext sqlite3 \
+	python-lxml \
+	postgresql-client libpq-dev python-psycopg2 \
 	python-imaging \
-	python-dev python-ldap \
-	libmemcached-dev libsasl2-dev zlib1g-dev \
+	python-ldap \
+	libmemcached-dev libsasl2-dev \
 	python-pylibmc \
+	gdal-bin libgdal-dev libgeos-dev \
 	--no-install-recommends
-
-# install stable postgres client
-touch /etc/apt/sources.list.d/pgdg.list
-echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" >>/etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-apt-get update && apt install postgresql-client libpq-dev python-psycopg2 -y
 
 # update python pip version
 pip install --upgrade pip
 
-# add gdal debian repo
-echo "deb http://http.us.debian.org/debian buster main non-free contrib" >>/etc/apt/sources.list
-apt autoclean -y && apt autoremove -y && apt update -y
-apt-get install -y gdal-bin libgdal-dev libgeos-dev
-
 # install python gdal stable
 pip install GDAL==2.3.2
+
+# install geonode from commit hash if dev enabled
 if [ "$GEONODE_DEV" = true ]; then
 	git clone https://github.com/GeoNode/geonode.git &&
 		cd /geonode && git reset --hard ${GEONODE_SHA1} && pip install . &&
@@ -42,9 +43,12 @@ mkdir -p ${APP_DIR}
 cd /cartoview && pip install . && rm -rf /cartoview
 
 # cleanup image
-apt autoremove --purge -y && apt autoclean -y
 rm -rf ~/.cache/pip
-rm -rf /var/lib/apt/lists/* && apt-get clean &&
+rm -rf /root/.cache
+apt-get purge --auto-remove -y gcc libgdal-dev libsasl2-dev \
+	zlib1g-dev python-dev build-essential
+apt autoremove --purge -y && apt autoclean -y && apt-get clean -y
+rm -rf /var/lib/apt/lists/* && apt-get clean -y &&
 	rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 echo "Yes, do as I say!" | apt-get remove --force-yes login &&
-	dpkg --remove --force-depends wget unzip
+	dpkg --remove --force-depends wget
