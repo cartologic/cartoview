ebe0d65adc5b19d6571f50b895946556db9d2841
hisham waleed karam <hisham.karam@cartologic.com>
2019-03-03 12:43:08 +0200

    add  install and uninstall endpoints

--- cartoview/api/views.py
+++ cartoview/api/views.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from cartoview.app_manager.exceptions import AppAlreadyInstalledException
+from cartoview.app_manager.installer import AppInstaller
 from cartoview.app_manager.models import App, AppStore, AppType
+from cartoview.log_handler import get_logger
+from pkg_resources import parse_version
+
 from .permissions import AppPermission
 from .serializers import (AppSerializer, AppStoreSerializer, AppTypeSerializer,
                           UserSerializer)
-from rest_framework import permissions
+
+logger = get_logger(__name__)
 
 
 class AppStoreViewSet(viewsets.ModelViewSet):
@@ -31,6 +40,53 @@ class AppViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(installed_by=self.request.user)
+
+    @action(detail=False, methods=['post'],
+            permission_classes=[permissions.IsAdminUser])
+    def install(self, request):
+        data = request.data
+        app_name = data.get('app_name', None)
+        store_id = data.get('store_id', None)
+        version = data.get('app_version', None)
+        if not app_name or not store_id or not version:
+            return Response({"details": "invalid data"}, status=400)
+        qs = App.objects.filter(name=app_name)
+        if qs.count() > 0:
+            permitted = request.user.has_perm('install_app', qs.first())
+            if not permitted:
+                return Response({"details":
+                                 "You don't have permission to install <> app".
+                                 format(app_name)}, status=403)
+            if parse_version(version) <= parse_version(qs.first().version):
+                return Response({"details": "app already installed"}, status=400)
+
+        try:
+            installer = AppInstaller(
+                app_name, store_id, version, user=request.user)
+            installer.install()
+        except BaseException as e:
+            if isinstance(e, AppAlreadyInstalledException):
+                logger.warn(e)
+            return Response({"details": str(e)}, status=500)
+        serializer = AppSerializer(App.objects.get(name=app_name))
+        return Response(serializer.data, status=200)
+
+    @action(detail=True, methods=['get'],
+            permission_classes=[AppPermission, permissions.IsAdminUser])
+    def uninstall(self, request, pk=None):
+        try:
+            app = App.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return Response("App Not Found to uninstall", status=404)
+        try:
+            installer = AppInstaller(
+                app.name, app.store.id, app.version, user=request.user)
+            installer.uninstall()
+        except BaseException as e:
+            return Response({"details": str(e)}, status=500)
+        print(app)
+        serializer = AppSerializer(app)
+        return Response(serializer.data, status=200)
 
 
 class UserViewSet(viewsets.ModelViewSet):
