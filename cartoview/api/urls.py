fc988759d7351e2d788846c5c1a8aca78d7cdf56
hisham waleed karam <hisham.karam@cartologic.com>
2019-03-03 12:44:32 +0200

    update api urls

--- cartoview/api/urls.py
+++ cartoview/api/urls.py
@@ -8,11 +8,10 @@ from .views import AppStoreViewSet, AppTypeViewSet, AppViewSet, UserViewSet
 schema_view = get_swagger_view(title='Cartoview API')
 app_name = 'api'
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'apps', AppViewSet)
-router.register(r'apptypes', AppTypeViewSet)
-router.register(r'stores', AppStoreViewSet)
-
+router.register(r'users', UserViewSet, 'users')
+router.register(r'apps', AppViewSet, 'apps')
+router.register(r'apptypes', AppTypeViewSet, 'apptypes')
+router.register(r'stores', AppStoreViewSet, 'stores')
 urlpatterns = ([
     re_path(r'^swagger$', schema_view),
     re_path(r'^', include(router.urls)),
