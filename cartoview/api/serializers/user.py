3a6191954e3f562a38e026e8264ae46522019877
hisham waleed karam <hisham.karam@cartologic.com>
2019-03-15 17:48:52 +0200

    use password field in users api

--- cartoview/api/serializers/user.py
+++ cartoview/api/serializers/user.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
+
+class PasswordField(serializers.CharField):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        return make_password(data)
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
+    password = PasswordField(
         write_only=True,
         required=True,
         help_text='Leave empty if no change needed',
