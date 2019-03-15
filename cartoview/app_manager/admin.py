ef1c31720c6e14bde12af22ac09ccb039d961803
Mohamed Salah <msalah593@gmail.com>
2019-03-15 17:27:11 +0200

    add Guarded admin to apps

--- cartoview/app_manager/admin.py
+++ cartoview/app_manager/admin.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from .models import App, AppStore, AppType, AppInstance
+from guardian.admin import GuardedModelAdmin
 # Register your models here.
 
 
 @admin.register(App)
-class AppModelAdmin(admin.ModelAdmin):
+class AppModelAdmin(GuardedModelAdmin):
     pass
 
 
