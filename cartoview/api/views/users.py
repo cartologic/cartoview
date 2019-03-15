acfc17e22fc5affc9425d667d6c1d056bb098c23
Mohamed Salah <msalah593@gmail.com>
2019-03-15 14:38:05 +0200

    adding api user permissions

--- cartoview/api/views/users.py
+++ cartoview/api/views/users.py
@@ -3,8 +3,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 
 from ..serializers.user import UserSerializer
+from ..permissions import UserPermission
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (UserPermission,)
