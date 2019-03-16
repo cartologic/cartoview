2db07e4aa552c76aaa7fd5692c2921ad567cae03
Mohamed Salah <msalah593@gmail.com>
2019-03-17 00:06:48 +0200

    adding testcases for user update api

--- cartoview/api/tests.py
+++ cartoview/api/tests.py
@@ -5,11 +5,11 @@ from django.contrib.auth import get_user_model
 # Create your tests here.
 
 
-class UserTests(APITestCase):
+class UserCreateTests(APITestCase):
+    fixtures = ['users_data']
 
     def test_create_user(self):
         count = get_user_model().objects.count()
-        print(count)
         response = self.client.post(reverse(
             'api:users-list'), {'username': 'apitester',
                                 'password': 'api@1234'})
@@ -37,3 +37,52 @@ class UserTests(APITestCase):
                                 'password': 'apitester1'})
         self.assertNotEqual(count+1, get_user_model().objects.count())
         self.assertEqual(response.status_code, 400)
+
+
+class UserUpdateTests(APITestCase):
+    fixtures = ['users_data']
+
+    def get_update_url(self):
+        url = reverse('api:users-detail', kwargs={'pk': 4})
+        return url
+
+    def get_updated_user(self):
+        return get_user_model().objects.get(pk=4)
+
+    def test_update_user_admin(self):
+        self.client.login(username='admin', password='admin@1234')
+        self.client.patch(self.get_update_url(), {'username': 'apitester',
+                                                  'email': 'mail@test.com'})
+        updated = self.get_updated_user()
+        self.assertEqual(updated.username, 'apitester')
+        self.assertEqual(updated.email, 'mail@test.com')
+
+    def test_update_user_owner(self):
+        self.client.login(username='owneruser', password='edit@1234')
+        self.client.patch(self.get_update_url(), {'username': 'apitester',
+                                                  'email': 'mail@test.com'})
+        updated = self.get_updated_user()
+        self.assertEqual(updated.username, 'apitester')
+        self.assertEqual(updated.email, 'mail@test.com')
+
+    def test_update_user_unauthenticated(self):
+        self.client.patch(self.get_update_url(), {'username': 'apitester',
+                                                  'email': 'mail@test.com'})
+        updated = self.get_updated_user()
+        self.assertNotEqual(updated.email, 'mail@test.com')
+
+    def test_update_user_non_owner(self):
+        self.client.login(username='nonowneruser', password='temp@1234')
+        self.client.patch(self.get_update_url(), {'username': 'apitester',
+                                                  'email': 'mail@test.com'})
+        updated = self.get_updated_user()
+        self.assertNotEqual(updated.username, 'apitester')
+        self.assertNotEqual(updated.email, 'mail@test.com')
+
+
+class UserListTests(APITestCase):
+    fixtures = ['users_data']
+
+    def test_list_users(self):
+        response = self.client.get(reverse('api:users-list'))
+        self.assertEqual(response.status_code, 200)
