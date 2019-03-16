7cc97bfd353430c2f695c7c55f46dcbdf563c421
hisham waleed karam <hisham.karam@cartologic.com>
2019-03-17 01:26:45 +0200

    complete apps loading

--- cartoview/app_manager/urls.py
+++ cartoview/app_manager/urls.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
-from django.urls import re_path
+from django.urls import include, re_path
+
+from .config import CartoviewApp
 from .views import ManageAppsView
+
 app_name = 'app_manager'
-urlpatterns = ([
+urlpatterns = [
     re_path('manage', ManageAppsView.as_view(), name='manage-apps'),
-], 'app_manager')
+]
+
+
+def app_url(app_name):
+    app = str(app_name)
+    return re_path(
+        r'^{app_name}/'.format(app_name=app),
+        include('%s.urls' % app), name='%s_base_url' % app)
+
+
+def load_apps_urls():
+    app_names = CartoviewApp.objects.get_active_apps().keys()
+    for app_name in app_names:
+        urlpatterns.append(app_url(app_name))
+
+
+load_apps_urls()
--- cartoview/settings.py
+++ cartoview/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import sys
+from cartoview.log_handler import get_logger
+logger = get_logger(__name__)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -260,4 +262,26 @@ CARTOVIEW_CONNECTIONS = {
 try:
     from .local_settings import *  # noqa
 except BaseException as e:
-    print(e)
+    logger.error(str(e))
+
+# NOTE: load cartoview apps
+if APPS_DIR not in sys.path:
+    sys.path.append(APPS_DIR)
+from cartoview.app_manager.config import CartoviewApp  # noqa
+CartoviewApp.load(apps_dir=APPS_DIR)
+for app in CartoviewApp.objects.get_active_apps().values():
+    try:
+        # ensure that the folder is python module
+        app_module = __import__(app.name)
+        app_dir = os.path.dirname(app_module.__file__)
+        app_settings_file = os.path.join(app_dir, 'settings.py')
+        libs_dir = os.path.join(app_dir, 'libs')
+        if os.path.exists(app_settings_file):
+            app_settings_file = os.path.realpath(app_settings_file)
+            exec(open(app_settings_file).read())
+        if os.path.exists(libs_dir) and libs_dir not in sys.path:
+            sys.path.append(libs_dir)
+        if app.name not in INSTALLED_APPS:
+            INSTALLED_APPS += (app.name.__str__(), )
+    except Exception as e:
+        logger.error(str(e))
