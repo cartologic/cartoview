66e70a27baf78c3881e29d037dedef55ff691b98
Mohamed Salah <msalah593@gmail.com>
2019-03-16 14:46:44 +0200

    adding password validation on user api
    had to revert back Serializer password field as it validates on the hashed password

--- cartoview/api/serializers/user.py
+++ cartoview/api/serializers/user.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
-
-
-class PasswordField(serializers.CharField):
-    def to_representation(self, value):
-        return value
-
-    def to_internal_value(self, data):
-        return make_password(data)
+import django.contrib.auth.password_validation as validators
+from django.core import exceptions
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = PasswordField(
+    password = serializers.CharField(
         write_only=True,
         required=True,
         help_text='Leave empty if no change needed',
@@ -24,3 +17,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'username', 'email', 'first_name',
                   'last_name', 'date_joined', 'groups', 'password')
+
+    def validate(self, data):
+        # get the password from the data
+        username = data.get('username')
+        password = data.get('password')
+        User = get_user_model()
+        user = User(username=username, password=password)
+        errors = dict()
+        try:
+            # validate the password and catch the exception
+            validators.validate_password(password=password, user=user)
+
+        # the exception raised here different than serializers.ValidationError
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super(UserSerializer, self).validate(data)
