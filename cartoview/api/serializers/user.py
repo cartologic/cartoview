3c52b9da05b8e95a7ea572b4ee9c4dca1f72ee72
Mohamed Salah <msalah593@gmail.com>
2019-03-16 14:46:44 +0200

    Revert "adding password validation on user api"
    This reverts commit 66e70a27baf78c3881e29d037dedef55ff691b98.

--- cartoview/api/serializers/user.py
+++ cartoview/api/serializers/user.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-import django.contrib.auth.password_validation as validators
-from django.core import exceptions
+from django.contrib.auth.hashers import make_password
+
+
+class PasswordField(serializers.CharField):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        return make_password(data)
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
+    password = PasswordField(
         write_only=True,
         required=True,
         help_text='Leave empty if no change needed',
@@ -17,23 +24,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'username', 'email', 'first_name',
                   'last_name', 'date_joined', 'groups', 'password')
-
-    def validate(self, data):
-        # get the password from the data
-        username = data.get('username')
-        password = data.get('password')
-        User = get_user_model()
-        user = User(username=username, password=password)
-        errors = dict()
-        try:
-            # validate the password and catch the exception
-            validators.validate_password(password=password, user=user)
-
-        # the exception raised here different than serializers.ValidationError
-        except exceptions.ValidationError as e:
-            errors['password'] = list(e.messages)
-
-        if errors:
-            raise serializers.ValidationError(errors)
-
-        return super(UserSerializer, self).validate(data)
--- cartoview/api/views/users.py
+++ cartoview/api/views/users.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
-from django.contrib.auth.hashers import make_password
+
 from ..serializers.user import UserSerializer
 from ..permissions import UserPermission
 
@@ -10,15 +10,3 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = (UserPermission,)
-
-    def perform_create(self, serializer):
-        serializer.validated_data['password'] = make_password(
-            serializer.validated_data['password'])
-        serializer.save()
-
-    def perform_update(self, serializer):
-        data = serializer.validated_data
-        if data.get('password', None):
-            serializer.validated_data['password'] = make_password(
-                serializer.validated_data['password'])
-        serializer.save()
