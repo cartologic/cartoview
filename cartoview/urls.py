c831b13699ef8de71b5ab9b58619915a79a4482e
hisham waleed karam <hisham.karam@cartologic.com>
2019-03-26 00:49:10 +0200

    add static urls in development

--- cartoview/urls.py
+++ cartoview/urls.py
@@ -43,3 +43,5 @@ urlpatterns += [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
