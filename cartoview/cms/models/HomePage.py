dc666938a381ab375345db2778cb99ad28d5e531
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2019-04-12 10:08:59 +0200

    Dynamic Template support for Home Page

--- cartoview/cms/models/HomePage.py
+++ cartoview/cms/models/HomePage.py
@@ -1,5 +1,7 @@
+from django import forms
+from django.db import models
 from django.utils.safestring import mark_safe
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel, TabbedInterface, ObjectList
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
@@ -12,7 +14,7 @@ from .Blocks import HeroAreaBlock
 class HomePage(Page):
     hero = StreamField([
         ('hero_area', HeroAreaBlock()),
-        ], blank=True, null=True, help_text=mark_safe("You should add only <b>1 Hero</b>"))
+    ], blank=True, null=True, help_text=mark_safe("You should add only <b>1 Hero</b>"))
     body = StreamField([
         ('grid', GridBlock()),
         ('header', HeaderBlock()),
@@ -22,8 +24,26 @@ class HomePage(Page):
         ('list_with_images', ListWithImagesBlock()),
         ('thumbnail_gallery', ThumbnailGalleryBlock()),
     ], blank=True, null=True)
+    selected_template = models.CharField(max_length=255, choices=(
+        ('cms/home_page_default.html', 'Default Template'),
+    ), default='cms/home_page_default.html')
 
-    content_panels = Page.content_panels + [
+    @property
+    def template(self):
+        return self.selected_template
+
+    content_panels = [
+        FieldPanel('title', classname="full title"),
         StreamFieldPanel("hero", classname="Full"),
         StreamFieldPanel("body", classname="Full"),
     ]
+    theme_panels = [
+        FieldPanel('selected_template', widget=forms.Select),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Content'),
+        ObjectList(theme_panels, heading='Theme'),
+        ObjectList(Page.promote_panels, heading='Promote'),
+        ObjectList(Page.settings_panels, heading='Settings', classname="settings"),
+    ])
