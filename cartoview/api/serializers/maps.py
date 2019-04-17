8652d85410051309338fb2a4a5069010b4d6b9a0
hisham waleed karam <hisham.karam@cartologic.com>
2019-04-17 03:28:43 +0200

    add list field

--- cartoview/api/serializers/maps.py
+++ cartoview/api/serializers/maps.py
@@ -1,27 +1,12 @@
-import json
-
 from rest_framework import serializers
-
+from cartoview.fields import ListSerializerField
 from cartoview.maps.models import Map
 
 from .base_resource import BaseModelSerializer
 
 
-class CenterField(serializers.CharField):
-
-    def to_representation(self, value):
-        data = None
-        data = json.loads(value)
-        return data
-
-    def to_internal_value(self, data):
-        if isinstance(data, list):
-            return json.dumps(data)
-        return data
-
-
 class MapSerializer(BaseModelSerializer):
-    center = CenterField()
+    center = ListSerializerField()
     owner = serializers.StringRelatedField(
         read_only=True, default=serializers.CurrentUserDefault())
 
