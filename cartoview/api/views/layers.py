4449115966339bcdd38b80a603eb493ea42ea8b7
hisham waleed karam <hisham.karam@cartologic.com>
2019-04-23 11:35:46 +0200

    fix layers api

--- cartoview/api/views/layers.py
+++ cartoview/api/views/layers.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions, viewsets
-
+from django_filters.rest_framework import DjangoFilterBackend
 from cartoview.layers.models import Layer
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 from ..filters import DjangoObjectPermissionsFilter, LayerFilter
 from ..serializers.layers import LayerSerializer
 
@@ -11,7 +11,8 @@ class LayerViewSet(viewsets.ModelViewSet):
     serializer_class = LayerSerializer
     filterset_class = LayerFilter
     permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
-    filter_backends = (DjangoObjectPermissionsFilter,)
+    filter_backends = (DjangoObjectPermissionsFilter,
+                       DjangoFilterBackend, OrderingFilter, SearchFilter)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
