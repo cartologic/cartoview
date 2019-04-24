abd60d8c05876d791b03334e34765ed840dc4a1f
hisham waleed karam <hisham.karam@cartologic.com>
2019-04-24 07:47:06 +0200

    fix api users test

--- cartoview/api/tests/test_users.py
+++ cartoview/api/tests/test_users.py
@@ -1,89 +1,31 @@
-from django.contrib.auth import get_user_model
+import pytest
 from django.shortcuts import reverse
+from faker import Faker
+from rest_framework import status
 from rest_framework.test import APITestCase
 
-# Create your tests here.
+fake = Faker()
 
 
+@pytest.mark.django_db
+@pytest.mark.usefixtures("test_initial_users")
 class UserCreateTests(APITestCase):
-    fixtures = ['users_data']
+    def get_valid_user_data(self):
+        return {'username': fake.user_name(),
+                'email': fake.email(),
+                'password': fake.password()}
+
+    def get_invalid_user_data(self):
+        return {'username': fake.user_name(),
+                'email': fake.address(),
+                'password': fake.numerify()}
 
     def test_create_user(self):
-        count = get_user_model().objects.count()
         response = self.client.post(reverse(
-            'api:users-list'), {'username': 'apitester',
-                                'password': 'api@1234'})
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(count+1, get_user_model().objects.count())
+            'api:users-list'), self.get_valid_user_data())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_user_invalid_password(self):
-        count = get_user_model().objects.count()
-        self.client.post(reverse(
-            'api:users-list'), {'username': 'apitester',
-                                'password': '234'})
-        self.assertNotEqual(count+1, get_user_model().objects.count())
-
-    def test_create_user_invalid_mail(self):
-        count = get_user_model().objects.count()
-        self.client.post(reverse(
-            'api:users-list'), {'username': 'apitester',
-                                'email': 'apiappp.com',
-                                'password': 'api@1234'})
-        self.assertNotEqual(count+1, get_user_model().objects.count())
-
-    def test_create_user_invalid_same(self):
-        count = get_user_model().objects.count()
-        response = self.client.post(reverse(
-            'api:users-list'), {'username': 'apitester1',
-                                'password': 'apitester1'}, format='json')
-        self.assertNotEqual(count+1, get_user_model().objects.count())
-        self.assertEqual(response.status_code, 400)
-
-
-class UserUpdateTests(APITestCase):
-    fixtures = ['users_data']
-
-    def get_update_url(self):
-        url = reverse('api:users-detail', kwargs={'pk': 4})
-        return url
-
-    def get_updated_user(self):
-        return get_user_model().objects.get(pk=4)
-
-    def test_update_user_admin(self):
-        self.client.login(username='admin', password='admin@1234')
-        self.client.patch(self.get_update_url(), {'username': 'apitester',
-                                                  'email': 'mail@test.com'})
-        updated = self.get_updated_user()
-        self.assertEqual(updated.username, 'apitester')
-        self.assertEqual(updated.email, 'mail@test.com')
-
-    def test_update_user_owner(self):
-        self.client.login(username='owneruser', password='edit@1234')
-        self.client.patch(self.get_update_url(), {'username': 'apitester',
-                                                  'email': 'mail@test.com'})
-        updated = self.get_updated_user()
-        self.assertEqual(updated.username, 'apitester')
-        self.assertEqual(updated.email, 'mail@test.com')
-
-    def test_update_user_unauthenticated(self):
-        self.client.patch(self.get_update_url(), {'username': 'apitester',
-                                                  'email': 'mail@test.com'})
-        updated = self.get_updated_user()
-        self.assertNotEqual(updated.email, 'mail@test.com')
-
-    def test_update_user_non_owner(self):
-        self.client.login(username='nonowneruser', password='temp@1234')
-        self.client.patch(self.get_update_url(), {'username': 'apitester',
-                                                  'email': 'mail@test.com'})
-        updated = self.get_updated_user()
-        self.assertNotEqual(updated.username, 'apitester')
-        self.assertNotEqual(updated.email, 'mail@test.com')
-
-
-class UserListTests(APITestCase):
-    fixtures = ['users_data']
-
-    def test_list_users(self):
-        response = self.client.get(reverse('api:users-list'))
-        self.assertEqual(response.status_code, 200)
+    def test_create_invalid_user(self):
+        resp = self.client.post(reverse(
+            'api:users-list'), self.get_invalid_user_data())
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
--- cartoview/api/urls.py
+++ cartoview/api/urls.py
@@ -16,15 +16,15 @@ app_name = 'api'
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, 'users')
 router.register(r'apps', AppViewSet, 'apps')
-router.register(r'apptypes', AppTypeViewSet, 'apptypes')
+router.register(r'apptypes', AppTypeViewSet, 'app_types')
 router.register(r'stores', AppStoreViewSet, 'stores')
 router.register(r'servers', ServerViewSet, 'servers')
 router.register(r'layers', LayerViewSet, 'layers')
 router.register(r'maps', MapViewSet, 'maps')
 router.register(r'bookmarks', BookmarkViewSet, 'bookmarks')
 router.register(r'appinstance', AppInstanceViewSet, 'appinstance')
-router.register(r'simpleauth', SimpleAuthConnectionViewSet, 'simpleauth')
-router.register(r'tokenauth', TokenAuthConnectionViewSet, 'tokenauth')
+router.register(r'simple_auth', SimpleAuthConnectionViewSet, 'simple_auth')
+router.register(r'token_auth', TokenAuthConnectionViewSet, 'token_auth')
 urlpatterns = [
     re_path(r'^swagger$', schema_view),
     re_path(r'^', include(router.urls)),
