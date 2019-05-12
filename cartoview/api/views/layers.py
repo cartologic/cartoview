241e98f9c5b5b78d52fe26ae3940723bb1479cc0
hisham waleed karam <hisham.karam@cartologic.com>
2019-05-12 13:56:50 +0200

    add attributes api

--- cartoview/api/views/layers.py
+++ cartoview/api/views/layers.py
@@ -1,9 +1,26 @@
-from rest_framework import permissions, viewsets
-from django_filters.rest_framework import DjangoFilterBackend
 from cartoview.layers.models import Layer
-from rest_framework.filters import SearchFilter, OrderingFilter
+from django.core.exceptions import ObjectDoesNotExist
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
+
 from ..filters import DjangoObjectPermissionsFilter, LayerFilter
 from ..serializers.layers import LayerSerializer
+
+
+def build_describe_feature_type(layer):
+    params = {'service': 'wfs',
+              'version': '2.0.0',
+              'request': 'DescribeFeatureType',
+              'typeNames': layer.name,
+              'outputFormat': 'application/json'}
+    handler = layer.server.handler
+    session = handler.session
+    req = session.get(layer.server.url, params=params)
+    return req
 
 
 class LayerViewSet(viewsets.ModelViewSet):
@@ -16,3 +33,23 @@ class LayerViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=['get'], url_name='attributes')
+    def attributes(self, request, pk=None):
+        try:
+            obj = Layer.objects.get(pk=pk)
+        except ObjectDoesNotExist as e:
+            raise NotFound(str(e))
+        layer_type = obj.server.server_type
+        if layer_type == "OGC-WFS":
+            try:
+                response = build_describe_feature_type(obj)
+                data = response.json()
+                featureTypes = data.get('featureTypes')
+                featureTypes = list(filter(lambda d: d.get('typeName') ==
+                                           obj.name.split(":")[1], featureTypes))
+                if len(featureTypes) > 0:
+                    return Response(featureTypes[0], status=200)
+            except BaseException as e:
+                return Response({'details': str(e)}, status=500)
+        return Response({}, status=200)
