45defc327fc8f13cddf7b5429ef92fcf9c63470d
hisham waleed karam <hisham.karam@cartologic.com>
2019-05-14 16:02:33 +0200

    refactor cms and add apps page

---
+++ cartoview/cms/db_models/app_manager/apps_list.py
@@ -0,0 +1,42 @@
+from cartoview.app_manager.models import App
+from django import forms
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
+
+class AppChooserblock(blocks.ChooserBlock):
+    target_model = App
+    widget = forms.Select
+
+    def value_for_form(self, value):
+        if isinstance(value, self.target_model):
+            return value.pk
+        else:
+            return value
+
+
+class AppsPage(Page):
+    heading = models.CharField(max_length=255, blank=False, null=False, default="Apps")
+    abstract = models.TextField(blank=True, null=True)
+    apps = StreamField(
+        [('apps', AppChooserblock(required=True),)], blank=True
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel('heading'),
+        FieldPanel('abstract', classname="full"),
+        StreamFieldPanel('apps'),
+    ]
+    @property
+    def apps_list(self):
+        apps = [app.value for app in self.apps]
+        if len(apps) == 0:
+            apps = App.objects.all()
+        return apps
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        print(context)
+        return context
--- cartoview/cms/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .HomePage import HomePage  # noqa
-from .map_catalog.MapCatalogPage import MapCatalogPage  # noqa
-from .map_catalog.MapCatalogKeywordIndex import MapCatalogKeywordIndex  # noqa
-from .map_catalog.MapCatalogSearchIndex import MapCatalogSearchIndex  # noqa
-
-from .menu.MenuItem import MenuItem  # noqa
-from .menu.MenuLink import MenuLink  # noqa
-
-from .settings.SiteSettings import SiteSettings  # noqa
