22e888e31c52fcaca057bdc86473bc8b0ba9f861
hisham waleed karam <hisham.karam@cartologic.com>
2019-05-22 20:45:51 +0200

    add more attributes to layers model

--- cartoview/layers/models.py
+++ cartoview/layers/models.py
@@ -19,9 +19,9 @@ from .validators import validate_projection
 
 class BaseLayer(BaseModel):
     name = models.CharField(max_length=255)
-    bounding_box = ListField(null=False, blank=False, default=[0, 0, 0, 0],
-                             validators=[ListValidator(min_length=4,
-                                                       max_length=4), ])
+    attribution = models.TextField(null=True, blank=True)
+    bounding_box = ListField(null=False, blank=False, default=[0, 0, 0, 0], validators=[
+                             ListValidator(min_length=4, max_length=4), ])
     projection = models.CharField(max_length=30, blank=False, null=False,
                                   validators=[validate_projection, ])
     server = models.ForeignKey(Server, on_delete=models.CASCADE,
@@ -30,8 +30,15 @@ class BaseLayer(BaseModel):
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
                               related_name="layers", blank=True, null=True)
     url = models.URLField(verbose_name='layer URL', null=True, blank=True)
+    tile_size = ListField(null=False, blank=False, default=[256, 256], validators=[
+                          ListValidator(min_length=2, max_length=2), ])
+    urls = ListField(null=True, blank=True)
+    wrap_x = models.BooleanField(help_text="Whether to wrap the world horizontally.", default=True)
+    is_base = models.BooleanField(default=False, null=False, blank=False)
     @property
     def server_type(self):
+        if not self.server:
+            return None
         return self.server.server_type
 
     @property
@@ -66,8 +73,10 @@ class BaseLayer(BaseModel):
 
     @property
     def layer_type(self):
-        server = get_server_by_value(self.server.server_type)
-        return server.type
+        if self.server:
+            server = get_server_by_value(self.server.server_type)
+            return server.type
+        return None
 
     class Meta:
         abstract = True
