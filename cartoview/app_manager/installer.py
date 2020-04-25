e123d6abb9f5bd581e5d3848e91bea7db51ecb38
khaled <bokae97@gmail.com>
2020-04-25 05:07:10 +0200

    fixed: could not app info for apps are not added yet in the app store

--- cartoview/app_manager/installer.py
+++ cartoview/app_manager/installer.py
@@ -77,7 +77,7 @@ class RestartHelper(object):
 
 
 def remove_unwanted(info):
-    dictionary = info.__dict__.get('_data', {})
+    dictionary = info.__dict__.get('_data', {}) if info else {}
     app_fields = [
         str(field.name)
         for field in sorted(App._meta.fields + App._meta.many_to_many)
@@ -139,7 +139,7 @@ class AppInstaller(object):
 
     def get_app_version(self):
         if not self.version or self.version == 'latest' or \
-                self.info.latest_version.version == self.version:
+                (self.info and self.info.latest_version.version == self.version):
             self.version = self.info.latest_version
         else:
             data = self._request_rest_data("appversion/?app__name=", self.name,
