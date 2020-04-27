367b7e9083e175b5eb252bb8928e9e8c0515349a
khaled <bokae97@gmail.com>
2020-04-27 07:23:44 +0200

    added paver task install geoserver docker data dir

--- pavement.py
+++ pavement.py
@@ -533,3 +533,62 @@ def justcopy(origin, target):
         if not os.path.exists(target):
             os.makedirs(target)
         shutil.copy(origin, target)
+
+
+@task
+def install_docker_data_dir():
+    siteurl = os.environ.get('SITEURL', 'http://localhost/')
+    geoserver_location = os.environ.get('GEOSERVER_LOCATION', "http://geoserver:8080/")
+    nginx_location = os.environ.get("NGINX_LOCATION", "http://nginx:80/")
+
+    geoserver_data_dir = path('/geoserver_data/data')
+    global_conf = os.path.join(geoserver_data_dir, 'global.xml')
+    security_filter_conf = os.path.join(geoserver_data_dir, path('security/filter/geonode-oauth2/config.xml'))
+    security_role_conf = os.path.join(geoserver_data_dir, path('security/role/geonode REST role service/config.xml'))
+
+    try:
+        config = global_conf
+        with open(config) as f:
+            xml = f.read()
+            m = re.search('proxyBaseUrl>([^<]+)', xml)
+            xml = xml[:m.start(1)] + \
+                  "{}geoserver".format(geoserver_location) + xml[m.end(1):]
+            with open(config, 'w') as f:
+                f.write(xml)
+    except Exception as e:
+        print('Error while modifying {} :'.format(security_role_conf), e)
+
+    try:
+        config = security_filter_conf
+        with open(config) as f:
+            xml = f.read()
+            m = re.search('accessTokenUri>([^<]+)', xml)
+            xml = xml[:m.start(1)] + \
+                  "{}o/token/".format(nginx_location) + xml[m.end(1):]
+            m = re.search('userAuthorizationUri>([^<]+)', xml)
+            xml = xml[:m.start(
+                1)] + "{}o/authorize/".format(siteurl) + xml[m.end(1):]
+            m = re.search('redirectUri>([^<]+)', xml)
+            xml = xml[:m.start(
+                1)] + "{}geoserver/index.html".format(siteurl) + xml[m.end(1):]
+            m = re.search('checkTokenEndpointUrl>([^<]+)', xml)
+            xml = xml[:m.start(
+                1)] + "{}api/o/v4/tokeninfo/".format(nginx_location) + xml[m.end(1):]
+            m = re.search('logoutUri>([^<]+)', xml)
+            xml = xml[:m.start(
+                1)] + "{}account/logout/".format(siteurl) + xml[m.end(1):]
+            with open(config, 'w') as f:
+                f.write(xml)
+    except Exception as e:
+        print('Error while modifying {} :'.format(security_filter_conf), e)
+
+    try:
+        config = security_role_conf
+        with open(config) as f:
+            xml = f.read()
+            m = re.search('baseUrl>([^<]+)', xml)
+            xml = xml[:m.start(1)] + nginx_location[:-1] + xml[m.end(1):]
+            with open(config, 'w') as f:
+                f.write(xml)
+    except Exception as e:
+        print('Error while modifying {} :'.format(security_role_conf), e)
