3383e76866483de8816c247317c30116c8f4c2c0
khaled <bokae97@gmail.com>
2021-02-15 09:51:30 +0200

    cartoview urls remove geonode api urls

--- cartoview/urls.py
+++ cartoview/urls.py
@@ -2,26 +2,12 @@
 from __future__ import absolute_import
 
 from django.conf.urls import include, url
-from geonode.api.urls import api
 from geonode.urls import urlpatterns as geonode_urls
 
-from cartoview.app_manager.rest import AppResource, AppTypeResource
-from cartoview.cartoview_api.views import layer_config_json, update_extent
 from cartoview.views import check_version
 
-from .cartoview_api.rest import AllResourcesResource, ExtendedResourceBaseResource
-
-api.register(AppResource())
-api.register(AppTypeResource())
-api.register(AllResourcesResource())
-api.register(ExtendedResourceBaseResource())
 urlpatterns = [
-    url(r'^layer/(?P<layername>[^/]*)/json/?$',
-        layer_config_json, name='layer_json'),
-    url(r'^update/extent/(?P<typename>[^/]*)$', update_extent,
-        name='cartoview.update_extent'),
     url(r'^check-version/$', check_version, name='check_version'),
-    url(r'', include(api.urls)),
     url(r'^apps/', include('cartoview.app_manager.urls')),
 ]
 urlpatterns += geonode_urls
