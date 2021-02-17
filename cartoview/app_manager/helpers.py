a4de2fc8bf9a4f6a9d337d226173e60ad9dff5bf
khaled <bokae97@gmail.com>
2021-02-17 10:41:25 +0200

    mv create_apps_dir to app_manager.helpers.py

--- cartoview/app_manager/helpers.py
+++ cartoview/app_manager/helpers.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 import os
 import stat
+
+from django.conf import settings
+from future import standard_library
 
 from cartoview.log_handler import get_logger
 
 world_permission = 0o777
 logger = get_logger(__name__)
+
+standard_library.install_aliases()
 
 
 def create_direcotry(path, mode=0o777):
@@ -53,3 +62,14 @@ def make_writeable_recursive(path):
         for file in [os.path.join(root, f) for f in files]:
             os.chmod(file, get_perm(file) | stat.S_IRUSR |  # noqa
                      stat.S_IRGRP | stat.S_IROTH)
+
+
+def create_apps_dir(apps_dir=getattr(settings, 'APPS_DIR', None)):
+    if not apps_dir:
+        project_dir = getattr(settings, 'BASE_DIR', settings.PROJECT_DIR)
+        apps_dir = os.path.abspath(os.path.join(
+            os.path.dirname(project_dir), "apps"))
+    if not os.path.exists(apps_dir):
+        create_direcotry(apps_dir)
+        if not os.access(apps_dir, os.W_OK):
+            change_path_permission(apps_dir)
