ed5873e8a3efd6c923d5f5b02bc1b7993ef01d31
khaled <bokae97@gmail.com>
2021-02-28 06:57:57 +0200

    App instance model

--- cartoview/app_instances_catalog/models.py
+++ cartoview/app_instances_catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from model_utils.managers import InheritanceManager
 
-# Create your models here.
+
+class AppInstance(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    title = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True
+    )
+    abstract = models.TextField(
+        max_length=2000,
+        null=True,
+        blank=True
+    )
+    config = models.JSONField(
+        null=True,
+        blank=True
+    )
+
+    # model_utils InheritanceManager
+    objects = InheritanceManager()
