e1cfd8bc1efc06e704da5294c7403e762276f0e0
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2022-06-02 17:38:51 +0200

    fix setup.sh

--- scripts/database/setup.sh
+++ scripts/database/setup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 set -x
-psql -U postgres dropdb template_postgis
-psql -U postgres dropdb cartoview
-psql -U postgres dropdb cartoview_data
-psql -U postgres dropdb upload_test
-psql -U postgres dropdb test_upload_test
-psql -U postgres dropuser cartoview
-psql -U postgres createuser cartoview -d -s
+psql -U postgres -c "dropdb template_postgis"
+psql -U postgres -c "dropdb cartoview"
+psql -U postgres -c "dropdb cartoview_data"
+psql -U postgres -c "dropdb upload_test"
+psql -U postgres -c "dropdb test_upload_test"
+psql -U postgres -c "dropuser cartoview"
+psql -U postgres -c "createuser cartoview -d -s"
 psql -U postgres -c "ALTER USER cartoview WITH PASSWORD 'cartoview';"
-psql -U postgres createdb template_postgis
+psql -U postgres -c "createdb template_postgis"
 psql -U postgres -d template_postgis -c 'CREATE EXTENSION postgis;'
 psql -U postgres -d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
 psql -U postgres -d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
 psql -U postgres -d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO cartoview;'
-psql -U postgres createdb -O cartoview cartoview
-psql -U postgres createdb -T template_postgis -O cartoview cartoview_datastore
+psql -U postgres -c "createdb -O cartoview cartoview"
+psql -U postgres -c "createdb -T template_postgis -O cartoview cartoview_data"
