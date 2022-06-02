d103b099c1a9a2f6348a59ff1a74f27b27e9e0b5
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2022-06-02 18:02:14 +0200

    fix setup.sh

--- scripts/database/setup.sh
+++ scripts/database/setup.sh
@@ -7,12 +7,12 @@ psql -U postgres -c "drop database cartoview_data"
 psql -U postgres -c "drop database upload_test"
 psql -U postgres -c "drop database test_upload_test"
 psql -U postgres -c "drop user cartoview"
-psql -U postgres -c "create user cartoview -d -s"
+psql -U postgres -c "create user cartoview CREATEDB"
 psql -U postgres -c "ALTER USER cartoview WITH PASSWORD 'cartoview';"
 psql -U postgres -c "create database template_postgis"
 psql -U postgres -d template_postgis -c 'CREATE EXTENSION postgis;'
 psql -U postgres -d template_postgis -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
 psql -U postgres -d template_postgis -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
 psql -U postgres -d template_postgis -c 'GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO cartoview;'
-psql -U postgres -c "create database -O cartoview cartoview"
-psql -U postgres -c "create database -T template_postgis -O cartoview cartoview_data"
+psql -U postgres -c "create database cartoview cartoview template_postgis"
+psql -U postgres -c "create database cartoview_data cartoview template_postgis"
